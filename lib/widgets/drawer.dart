779c88fb1f9a5a03dc89fb2c280ac1220149fb97
DanialAhmed94 <danialuol@gmail.com>
2026-05-01 20:57:12 +0500

    publish on android initial release

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -149,7 +149,7 @@ class MyDrawer extends StatelessWidget {
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage(
-                'assets/images/drawerBackground.png'), // Change this to your image path
+                'assets/images/drawerBackground_updated.png'), // Change this to your image path
             fit: BoxFit.cover, // Control how the image scales
           ),
         ),
@@ -157,7 +157,7 @@ class MyDrawer extends StatelessWidget {
           children: [
             AppBar(
               title: Text(
-                "Hello Festival Toilet",
+                "Hello",
                 style: TextStyle(fontFamily: 'Poppins-Bold'),
               ),
               automaticallyImplyLeading: false,
@@ -313,119 +313,4 @@ class MyDrawer extends StatelessWidget {
   }
 }
 
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_svg/svg.dart';
-//
-// import '../screens/mainScreen.dart';
-// import '../screens/reviewsScreen.dart';
-//
-// class My_Drawer extends StatelessWidget {
-//   const My_Drawer({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Drawer(
-//       child: Column(
-//         children: [
-//           AppBar(
-//             title: Text(
-//               "Hello Crap Advisor",
-//               style: TextStyle(fontFamily: 'Poppins-Bold'),
-//             ),
-//             automaticallyImplyLeading: false,
-//           ),
-//           Divider(),
-//           ListTile(
-//             title: Text(
-//               "Home",
-//               style: TextStyle(fontFamily: 'Poppins-Medium'),
-//             ),
-//             leading: IconButton(
-//                 onPressed: () {
-//                   Navigator.push(context,
-//                       MaterialPageRoute(builder: (context) => MainScreen()));
-//                 },
-//                 icon: Icon(Icons.home_outlined)),
-//           ),
-//           SizedBox(
-//             height: 10,
-//           ),
-//           ListTile(
-//             title: Text(
-//               " Toilet Ratings",
-//               style: TextStyle(fontFamily: 'Poppins-Medium'),
-//             ),
-//             leading: IconButton(
-//               onPressed: () {
-//                 Navigator.push(context,
-//                     MaterialPageRoute(builder: (context) => Reviews()));
-//               },
-//               icon: Icon(Icons.dashboard_customize_outlined),
-//             ),
-//           ),
-//           SizedBox(
-//             height: 10,
-//           ),
-//           ListTile(
-//             title: Text(
-//               " Share App",
-//               style: TextStyle(fontFamily: 'Poppins-Medium'),
-//             ),
-//             leading: IconButton(onPressed: () {}, icon: Icon(Icons.share)),
-//           ),
-//           SizedBox(
-//             height: 10,
-//           ),
-//           ListTile(
-//             title: Text(
-//               " Feedback",
-//               style: TextStyle(fontFamily: 'Poppins-Medium'),
-//             ),
-//             leading: IconButton(
-//                 onPressed: () {}, icon: Icon(Icons.feedback_outlined)),
-//           ),
-//           SizedBox(
-//             height: 10,
-//           ),
-//           ListTile(
-//             title: Text(
-//               " Upcoming Products",
-//               style: TextStyle(fontFamily: 'Poppins-Medium'),
-//             ),
-//             leading: IconButton(
-//                 onPressed: () {}, icon: Icon(Icons.watch_later_outlined)),
-//           ),
-//           Spacer(),
-//           Row(
-//             children: [
-//               Padding(
-//                 padding: EdgeInsets.only(right: 8.0, left: 18.0),
-//                 // Adjust the right padding based on your preference
-//                 child: SvgPicture.asset(
-//                   'assets/svgs/logo-crap.svg',
-//                   height: 80,
-//                   width: 80,
-//                   fit: BoxFit.contain,
-//                 ),
-//               ),
-//               Text(
-//                 "Crap Adviser",
-//                 style: TextStyle(
-//                     fontSize: 16,
-//                     fontWeight: FontWeight.bold,
-//                     shadows: [
-//                       Shadow(
-//                         offset: Offset(2.0, 2.0),
-//                         blurRadius: 5.0,
-//                         color: Colors.black.withOpacity(0.5),
-//                       ),
-//                     ]),
-//               )
-//             ],
-//           )
-//         ],
-//       ),
-//     );
-//   }
-// }
+
